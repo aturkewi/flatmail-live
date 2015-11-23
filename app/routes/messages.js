03aec5991664231945db98849b48a5414cb10e73
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-23 11:01:09 -0500

    Add note

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
+    // 
     return Ember.$.getJSON('/api/v1/messages').then(function(response){
       return response.messages;
     });
