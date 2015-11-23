5e92c3ec1e32b1aa805e3f557dc432eed1748bf9
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-23 11:45:53 -0500

    All messages showing in broswer now

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    // 
-    return Ember.$.getJSON('/api/v1/messages').then(function(response){
-      return response.messages;
-    });
+    // store - responsible for makign requests to DB
+    return this.store.findAll('message');
+
+    // Get rid of old code after using the adapter.
+    // return Ember.$.getJSON('/api/v1/messages').then(function(response){
+    //   return response.messages;
+    // });
   }
 });
