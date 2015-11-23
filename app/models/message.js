fb911a244412baa27483d1667b1384b4ab0fe700
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-23 11:21:51 -0500

    Add model and adapters

---
+++ app/models/message.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  to: DS.attr('string'),
+  from: DS.attr('string'),
+  body: DS.attr('string'),
+  starred: DS.attr('boolean'),
+  archived: DS.attr('boolean')
+});
